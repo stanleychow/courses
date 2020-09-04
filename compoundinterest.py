4c2728ea369884a66cf4c718bd9045bb7d31b7ab
stanleychow <chowtech1022@gmail.com>
2020-09-04 12:02:10 -0400

    Updated print statement

--- compoundinterest.py
+++ compoundinterest.py
@@ -3,4 +3,4 @@ dollars = int(input("Dollars I want to invest: "))
 #Prompts user to input number of years as an integer
 years = int(input("Years I want to invest: "))
 #Prints and computes expected return given a 7 percent growth rate
-print(dollars * (pow(1.07, years)))
+print("Expected Return for 7 percent growth for " + str(dollars) + " dollars and " + str(years) + " years: " + str(dollars * (pow(1.07, years))))
