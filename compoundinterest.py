f02eb86c2e68343576525d6fe63d5716cb78200a
stanleychow <chowtech1022@gmail.com>
2020-09-04 11:53:50 -0400

    Updated Comments

--- compoundinterest.py
+++ compoundinterest.py
@@ -1,3 +1,6 @@
+#Prompts user to input dollar amount as an integer
 dollars = int(input("Dollars I want to invest: "))
+#Prompts user to input number of years as an integer
 years = int(input("Years I want to invest: "))
+#Prints and computes expected return given a 7 percent growth rate
 print(dollars * (pow(1.07, years)))
